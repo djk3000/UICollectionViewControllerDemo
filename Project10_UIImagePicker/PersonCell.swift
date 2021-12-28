9392dc2419df874ec8fa62352ef44c783d47ca42
djk3000 <djk3000djk@gmail.com>
2021-12-28 14:47:29 +0800

    -更新

---
+++ Project10_UIImagePicker/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10_UIImagePicker
+//
+//  Created by ruanjianyingyongbu on 2021/12/21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}
